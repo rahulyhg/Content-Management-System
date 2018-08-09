1ce87b3231d628f42204ce8f48b28122f1b43450
lucashogenboom <hogenboom456@gmail.com>
2018-08-09 22:48:35 +0200

    - index now shows small todo list

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
-<?php 
-
-echo 'Hello World!';
+<!DOCTYPE html>
+<html>
+	<head>
+		
+	</head>
+	<body>
+		<div class="to-do-list-containter">
+			<ul>
+				<li>1. Inlog scherm</li>
+				<li>2. Users om mee in te loggen</li>
+				<li>3. Database connectie</li>
+				<li>4. ..</li>
+				<li>5. ..</li>
+				<li>6. ..</li>
+			</ul>
+		</div>
+	</body>
+</html>
