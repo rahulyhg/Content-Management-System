0d3df4c9d687c218902b8d31163a2a46326d3414
lucashogenboom <hogenboom456@gmail.com>
2018-11-14 00:10:11 +0100

    - added a header

---
+++ layout/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" href="style/main.css"/>
+	</head>
+	<body>
+		<div id="main_outer_layer">
+			<header id="main_header">
+				<a href="?action=test_page" type="button">Press me</a>
+			</header>
+			<section id="main_content">
